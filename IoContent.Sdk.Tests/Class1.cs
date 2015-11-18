6f5aa531f04dc93ca5e5e3ba2322607010f17e99
Gareth Brown <gareth.brown@appsoftware.com>
2015-11-18 16:07:40 +0000

    Added NUnit framework and test project

---
+++ IoContent.Sdk.Tests/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace IoContent.Sdk.Tests
+{
+	[TestFixture]
+	public class IoContentSdkTests
+    {
+		[Test]
+		public void test_abc()
+		{
+			// Optionally pull content server side for example purposes
+
+			var contentClientBaseParameters = new ContentClientBaseParameters
+			{
+				ApiVersion = "v1.0",
+				ApiEndpointUrl = "https://iocontent.com",
+				SubAccountKey = "rvlzpmb7koytevscusj2f4ntpc",
+				ContentType = "test-article-a"
+			};
+
+			dynamic content;
+
+			using (var contentClient = new ContentClient(contentClientBaseParameters))
+			{
+				contentClient.ClearCache();
+
+				var contentList = contentClient.Get("?key.equals=r23gmukdmnbuuowk3ugrvxagac&markdownToHtml=true");
+
+				content = contentList.FirstOrDefault();
+			}
+
+			Assert.NotNull(content);
+			Assert.NotNull(content.Title);
+			Assert.NotNull(content.Content);
+		}
+    }
+}
